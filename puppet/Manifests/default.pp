a082ae06b32c67f2b477df5d38dcada0f5028e18
Ben Schwartz <ben.schwartz@bancvue.com>
2014-01-17 10:10:57 -0600

    dev(puppet): updating to deploy jekyll site

--- puppet/Manifests/default.pp
+++ puppet/Manifests/default.pp
@@ -1,1 +1,24 @@
-include java7
+Exec {
+        path => "/usr/bin:/usr/sbin:/bin:/sbin"
+}
+
+
+
+node default {
+
+
+        class { 'grid':
+                target_dir => '/var/www/jekyll-site',
+                repo_url => 'https://github.com/benschw/txt.fliglio.com.git',
+                branch => 'master',
+                app_name => 'my-app'
+        }
+
+}
+
+
+                $target_dir = '/var/www/jekyll-site',
+                $repo_url = 'https://github.com/benschw/txt.fliglio.com.git,
+                $branch = 'master',
+                $app_name = 'my-app'
+
